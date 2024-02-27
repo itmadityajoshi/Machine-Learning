03d0a6e8f694c15ebe07ea2b82b767aad82a0bd1
Aditya Joshi <adityajoshi316@gmail.com>
2024-02-27 19:39:09 +0545

    question no 3 done

---
+++ Week-2/Day-8/Exercise3.py
@@ -0,0 +1,25 @@
+''' create a program that analyzes a given text and 
+counts the freqyency of each unique word '''
+
+
+def count(user_input):
+    word = (user_input.split())
+    print(word)
+    word2 = [] #initalize empty list
+
+    #loop till string values present in list word2
+    for i in word:
+
+        # checking for hte duplicate
+        if i not in word2:
+
+            word2.append(i) #insert word in word2 
+    
+    for i in range(0, len(word2)):
+
+        # count the frequency of each word present in word2
+        print('Frequency of ', word2[i], 'is :', word.count(word2[i]))
+
+
+user_input = input("Enter the string for word count.")
+count(user_input)
